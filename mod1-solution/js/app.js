c60949ca6f9d4d88ba8add1226f949a880a35e31
David <dmaradiaga08@gmail.com>
2016-12-09 12:48:46 -0600

    Updating the app

--- mod1-solution/js/app.js
+++ mod1-solution/js/app.js
@@ -9,11 +9,37 @@
 
 		function LunchControllerFunction($scope){
 
+				$scope.msg = "";
+
 				$scope.userData = {};
 
 				$scope.clickme = function()
 				{
-						alert("HELLO WORLD")
+						if(!$scope.userData.someText)
+						{
+							  $scope.error = true;
+							  $scope.nice = false;
+							  $scope.msg = "Please enter data first"
+							  return
+						}
+						else{
+
+							 $scope.error = false;
+							 $scope.nice = true;
+							 $scope.items = $scope.userData.someText.split(",");
+						
+
+							 if($scope.items.length<=3)
+							 {
+							 		$scope.msg = "Enjoy!"
+							 }
+							 else{
+
+							 	  $scope.msg = "Too much!"
+							 }
+						}	
+
+
 				}
 		}
 })()
